564c4df1aa2a433527f3aa6e596694aecf32253a
Chengie14141 <chouchenghong@gmail.com>
2026-06-03 23:23:12 +0200

    Update AStarPathFinder.cs

--- Model/AStarPathFinder.cs
+++ Model/AStarPathFinder.cs
@@ -8,12 +8,84 @@ namespace Model
 
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions)
         {
-            //ToDo implement this method
-            visitedPositions.Enqueue(pos); //remove this line
+            // de grote van de maze
+            int rows = maze.MazeArray.Length;
+            int cols = maze.MazeArray[0].Length;
+
+            // setup
+            var Distance = new int[rows, cols];
+            var Previous = new int[rows, cols];
+            bool[,] VisitedNodes = new bool[rows, cols];
+
+            Distance[pos[0], pos[1]] = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Distance[row, col] = int.MaxValue;
+
+                    Previous[row, col] = -1;
+
+                    VisitedNodes[row, col] = false;
+                }
+            }
+
+            while(true)
+            {
+                int min = int.MaxValue;
+                int[] current = null!;
+
+                for(int row = 0; row < rows; row++)
+                {
+                    for(int col = 0; col < cols; col++)
+                    {
+                        int fScore = Distance[row, col] + Heuristic(row, col, maze);
+
+                        if (!VisitedNodes[row, col] && fScore < min)
+                        {
+                            min = fScore;
+                            current = new int[] { row, col };
+                        }
+                    }
+                }
+
+                if(current == null) break;
+
+                int Row = current[0];
+                int Col = current[1];
+
+                // zet als visited
+                VisitedNodes[Row, Col] = true;
+                visitedPositions.Enqueue(current);
+
+                if (Row == maze.End[0] && Col == maze.End[1]) break;
+
+                foreach(var move in maze.moves)
+                {
+                    int newCol = Col + move[0];
+                    int newRow = Row + move[1];
+
+                    if (!maze.IsValidMove(newCol, newRow))
+                        continue;
+
+                    if (VisitedNodes[newRow, newCol])
+                        continue;
+
+                    int newDistance = Distance[Row, Col] + 1;
+
+                    if (newDistance < Distance[newRow, newCol])
+                    {
+                        Distance[newRow, newCol] = newDistance;
+                        Previous[newRow, newCol] = Row * cols + Col;
+                    }
+                }
+            }
         }
 
+        public int Heuristic(int row, int col, Maze maze)
+        {
+            return Math.Abs(row - maze.End[0]) + Math.Abs(col - maze.End[1]);
+        }
     }
 }
-
-            
-
