b9d7fbf4fd4ebcd2b1921516f6afd2693817a54c
DeviN1111111 <1100961@hr.nl>
2026-06-04 16:08:23 +0200

    fixed recusrion added stack lower delay

--- Model/RecursivePathFinder.cs
+++ Model/RecursivePathFinder.cs
@@ -4,38 +4,28 @@ namespace Model
     {
         PathFinderType _algType = PathFinderType.Recursive;
         public PathFinderType algType { get => _algType; set {} }
-
+        private bool endFound = false;
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions)
         {
+            if(endFound) return; // als het eind al gevonden is, stop met zoeken
             int row = pos[0];
             int col = pos[1];
 
             if(!maze.IsValidMove(row, col)) return; // als je buiten de maze bent of tegen een muur aanloopt
-            if(AlreadyVisited(visitedPositions, pos)) return; // als je al op deze positie bent geweest
+            if(visitedPositions.Any(p => p[0] == pos[0] && p[1] == pos[1])) return; // als je al op deze positie bent geweest
 
             visitedPositions.Enqueue(pos);
 
-            if(AlreadyVisited(visitedPositions, maze.End)) return; // als je het eind hebt gevonden
+            if(pos[0] == maze.End[0] && pos[1] == maze.End[1])
+                endFound = true;
 
             foreach(var move in maze.moves)
             {
                 int newRow = row + move[0];
                 int newCol = col + move[1];
-
+            
                 FindPath(maze, new int[] { newRow, newCol }, visitedPositions);
-
-                if(AlreadyVisited(visitedPositions, maze.End)) return; // als je het eind hebt gevonden
             }
-        }
-
-        bool AlreadyVisited(Queue<int[]> visited, int[] pos) // checkt of je al op deze positie bent geweest
-        {
-            foreach (var p in visited)
-            {
-                if (p[0] == pos[0] && p[1] == pos[1])
-                    return true;
-            }
-            return false;
         }
     }
 }
--- Model/StackPathFinder.cs
+++ Model/StackPathFinder.cs
@@ -8,9 +8,31 @@ namespace Model
 
         public void FindPath(Maze maze, int[] pos, Queue<int[]> visitedPositions)
         {
-            //ToDo implement this method
-            visitedPositions.Enqueue(pos); //remove this line
-        }       
+            var visitedStack = new Stack<int[]>();
+            visitedStack.Push(pos);
+
+            
+            while(visitedStack.Count > 0)
+            {
+                int[] nextMove = visitedStack.Pop();
+
+                int row = nextMove[0];
+                int col = nextMove[1];
+
+                if(!maze.IsValidMove(row, col)) continue; // als je buiten de maze bent of tegen een muur aanloopt
+                if(visitedPositions.Any(p => p[0] == row && p[1] == col)) continue; // als je al op deze positie bent geweest
+                visitedPositions.Enqueue(nextMove);
+                if(row == maze.End[0] && col == maze.End[1]) return; // eind gevonden
+
+                foreach(var move in maze.moves)
+                {
+                    var newPosition = new int[] { row + move[0], col + move[1]};
+                    visitedStack.Push(newPosition);
+                
+                }
+            }
+            return;
+        }     
     }
 }
 
